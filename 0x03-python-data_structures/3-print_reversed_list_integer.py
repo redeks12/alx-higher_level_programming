a89c1dac443ab367ef0b0b5101b7fe2394978618
kumdan job <redeks123456@gmail.com>
2023-06-12 17:16:05 +0100

    high level

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
         b = len(my_list) - 1
         for i in my_list:
-                print(my_list[b])
+                print("{:d}".format(my_list[b]))
                 b -= 1
                 
                 
