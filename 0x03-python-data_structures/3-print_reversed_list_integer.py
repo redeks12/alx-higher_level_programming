d430c4911f729f201bdc5e32735120c848fa2158
kumdan job <redeks123456@gmail.com>
2023-06-12 17:17:52 +0100

    high level

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,9 +4,3 @@ def print_reversed_list_integer(my_list=[]):
         for i in my_list:
                 print("{:d}".format(my_list[b]))
                 b -= 1
-                
-                
-                
-
-my_list = [1,9 ,3,5,6,9,0,3,0,0,0,0,2, 3, 4, 5]
-print_reversed_list_integer(my_list)
