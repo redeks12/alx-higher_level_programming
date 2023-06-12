3ed5873ea0e94b12183639d7f3810e7246b05a68
kumdan job <redeks123456@gmail.com>
2023-06-12 19:27:21 +0100

    high level

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
         for i in matrix : 
                 for j in range(len(i)):
-                        if j < len(i):
+                        if j < len(i) -1:
                                 print("{:d}".format(i[j]), end=" ")
                         else:
                                 print("{:d}".format(i[j]), end="")
