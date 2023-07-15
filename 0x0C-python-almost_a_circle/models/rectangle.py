dd7146ef5d6fb32a285241d49bef1a8e489ba89b
kumdan job <redeks123456@gmail.com>
2023-07-15 11:13:36 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,35 +3,50 @@ from models.base import Base
 class Rectangle(Base):
         def __init__(self, width, height, x=0, y=0, id=None):
                 super().__init__(id)
-                self.set_height(height)
-                self.set_width(width)
-                self.set_x(x)
-                self.set_y(y)
+                self.height = height
+                self.width = width
+                self.y = y
+                self.x= x
+
+        @property
+        def width(self):
+                return self.__width
         
+        @width.setter
         def set_width(self, width):
-                if type(width) != int():
-                        raise TypeError("width must be an integer")
-                elif width <= 0:
+                self.valid_int(width, "width")
+                if width <= 0:
                         raise ValueError("width must be > 0")
                 self.__width = width
+        @property
+        def height(self):
+                return self.__height
         
+        @height.setter
         def set_height(self, height):
-                if type(height) != int():
-                        raise TypeError("height must be an integer")
-                elif height <= 0:
+                self.valid_int(height,"height")
+                if height <= 0:
                         raise ValueError("height must be > 0")
                 self.__height = height
-                
+        @property
+        def x(self):
+                return self.__x
+        @x.setter
         def set_x(self, x):
-                if type(x) != int():
-                        raise TypeError("x must be an integer")
-                elif x < 0:
+                self.valid_int(x, "x")
+                if x < 0:
                         raise ValueError("x must be >= 0")
                 self.__x = x
-        
+        @property
+        def y(self):
+                return self.__y
+        @y.setter
         def set_y(self, y):
-                if type(y) != int():
-                        raise TypeError("y must be an integer")
-                elif y < 0:
+                self.valid_int(y, "y")
+                if y < 0:
                         raise ValueError("y must be >= 0")
                 self.__y = y
+                
+        def valid_int(self, typ, name):
+                if type(typ) != int():
+                        raise TypeError("{} must be an integer".format(name))
