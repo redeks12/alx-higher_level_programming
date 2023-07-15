ad329ff312d0bb49dcbf96ca0ba34c437ae4c5e2
kumdan job <redeks123456@gmail.com>
2023-07-15 10:28:09 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from base import Base
+from models.base import Base
 class Rectangle(Base):
         def __init__(self, width, height, x=0, y=0, id=None):
                 super().__init__(id)
