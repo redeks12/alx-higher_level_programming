239bb0bd24967761fbaa72499523bb9279194a3a
kumdan job <redeks123456@gmail.com>
2023-07-16 09:08:47 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,61 +2,91 @@
 from models.base import Base
 class Rectangle(Base):
         def __init__(self, width, height, x=0, y=0, id=None):
-                '''Constructor'''
+                '''Constructor.'''
                 super().__init__(id)
-                self.height = height
                 self.width = width
+                self.height = height
+                self.x = x
                 self.y = y
-                self.x= x
 
         @property
         def width(self):
-                '''Get the width'''
+                '''Width of this rectangle.'''
                 return self.__width
-        
+
         @width.setter
-        def width(self, width):
-                '''Set the width'''
-                self.valid_int(width, "width")
-                if width <= 0:
-                        raise ValueError("width must be > 0")
-                self.__width = width
+        def width(self, value):
+                self.int_check("width", value, False)
+                self.__width = value
+
         @property
         def height(self):
-                '''Get the height'''
+                '''Height of this rectangle.'''
                 return self.__height
-        
+
         @height.setter
-        def height(self, height):
-                '''Set the height'''
-                self.valid_int(height,"height")
-                if height <= 0:
-                        raise ValueError("height must be > 0")
-                self.__height = height
+        def height(self, value):
+                self.int_check("height", value, False)
+                self.__height = value
+
         @property
         def x(self):
-                '''get x'''
+                '''x of this rectangle.'''
                 return self.__x
+
         @x.setter
-        def x(self, x):
-                '''set x'''
-                self.valid_int(x, "x")
-                if x < 0:
-                        raise ValueError("x must be >= 0")
-                self.__x = x
+        def x(self, value):
+                self.int_check("x", value)
+                self.__x = value
+
         @property
         def y(self):
-                '''get y'''
+                '''y of this rectangle.'''
                 return self.__y
+
         @y.setter
-        def y(self, y):
-                '''set y'''
-                self.valid_int(y, "y")
-                if y < 0:
-                        raise ValueError("y must be >= 0")
-                self.__y = y
+        def y(self, value):
+                self.int_check("y", value)
+                self.__y = value
+
+        def int_check(self, name, value, eq=True):
+                '''Method for validating the value.'''
+                if type(value) != int:
+                    raise TypeError("{} must be an integer".format(name))
+                if eq and value < 0:
+                    raise ValueError("{} must be >= 0".format(name))
+                elif not eq and value <= 0:
+                    raise ValueError("{} must be > 0".format(name))
+        def area(self):
+                return self.height * self.width
+        def display(self):
+                for _ in range(self.height):
+                        for _ in range(self.x):
+                                print(" ", end="")
+                        for _ in range(self.width):
+                                print("#",end="")
+                        print()
+        def __str__(self) -> str:
+               return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        
+        def __update(self, id=None, width=None, height=None, x=None, y=None): 
+                if id is not None:
+                        self.id = id
+                if width is not None:
+                        self.width = width
+                if height is not None:
+                        self.height = height
+                if x is not None:
+                        self.x = x
+                if y is not None:
+                        self.y = y
+
+        def update(self, *args, **kwargs):
+                if args:
+                        self.__update(*args)
+                else:
+                        self.__update(**kwargs)
                 
-        def valid_int(self, typ, name):
-                '''validate int type'''
-                if type(typ) != int():
-                        raise TypeError("{} must be an integer".format(name))
+        
+                        
+                        
