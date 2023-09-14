3716ac1eef12ca19b081e15c7516245571081e43
kumdan job <redeks123456@gmail.com>
2023-09-14 10:30:00 +0100

    m

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+)
+
+cursor = db.cursor()
+query = "SELECT name, id FROM states WHERE name LIKE 'N%' "
+cursor.execute(query)
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+cursor.close()
+db.close()
