698bcd63a2cfe13cf25c8c32b3af7eb8ec5fc909
kumdan job <redeks123456@gmail.com>
2023-09-14 17:40:52 +0100

    m

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,6 +4,7 @@ import sys
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
 
 # from model_state import State
 
@@ -11,18 +12,8 @@ engine = create_engine(
     "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]),
     pool_pre_ping=True,
 )
-Base = declarative_base()
 
-
-class State(Base):
-    __tablename__ = "states"
-    id = Column(
-        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
-    )
-    name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
 Session = sessionmaker()
 session = Session()
 
