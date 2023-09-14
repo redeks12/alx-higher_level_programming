15450eab2748b3b0221d2d9e4745750afac509ed
kumdan job <redeks123456@gmail.com>
2023-09-14 18:20:47 +0100

    m

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    pool_pre_ping=True,
+)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+state = (session.query(State).filter(State.id == 2)).first()
+state.name = "New Mexico"
+session.commit()
