713f972b1aa7068545e10e4c7b304de9d7e4b5bf
kumdan job <redeks123456@gmail.com>
2023-09-14 09:45:37 +0100

    ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+)
+
+cursor = db.cursor()
+query = "SELECT * FROM your_table"
+cursor.execute(query)
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
