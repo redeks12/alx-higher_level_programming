2005d9e30b5fc68d8df5b30598d6f22f5222b617
kumdan job <redeks123456@gmail.com>
2023-09-17 16:58:38 +0100

    m

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,8 +5,9 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import City, Base
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
 class State(Base):
