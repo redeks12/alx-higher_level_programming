f8391222ee87164c35005e8b136bfbf1de2d6b21
kumdan job <redeks123456@gmail.com>
2023-09-17 16:55:35 +0100

    m

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship("City", backref="state")
