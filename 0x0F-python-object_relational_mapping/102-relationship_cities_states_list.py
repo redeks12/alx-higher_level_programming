7a02d7cb289c644a1b47e4de1cd173e97206457a
kumdan job <redeks123456@gmail.com>
2023-09-17 23:04:19 +0100

    m

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this lists all the states in the given database"""
+
+import sys
+
+from relationship_city import Base, City
+from relationship_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(City).order_by(City.id).all()
+    for city in query:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
