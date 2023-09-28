db0024a080c3e6e028fc691e814afaa5dbe4e18a
kumdan job <redeks123456@gmail.com>
2023-09-28 17:38:58 +0100

    m

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to it, and displays the size of the body of the response
-curl sys.argv[1]
+curl $1
 # size_i=$(echo $response | jq -r 'Content-length')
