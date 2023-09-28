aec489cb4cff4185d9b0c3fd4a3e2c41614073af
kumdan job <redeks123456@gmail.com>
2023-09-28 23:43:42 +0100

    m

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -L -X PUT -d 'user_id=98' 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d 'user_id=98' 0.0.0.0:5000/catch_me
