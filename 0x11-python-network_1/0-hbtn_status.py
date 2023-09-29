546dca08ff9f2518274c0ddd767264b0279135de
kumdan job <redeks123456@gmail.com>
2023-09-29 10:23:33 +0100

    m

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,4 +3,6 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        print(response.read())
+        html = response.read()
+        for hh in html:
+            print("- {}".format(hh))
