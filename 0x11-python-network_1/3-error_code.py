1896f7f614ea756b49789a6b607ba38374a33fd4
kumdan job <redeks123456@gmail.com>
2023-09-29 11:11:04 +0100

    m

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,9 +3,10 @@ import sys
 from urllib import error, request
 
 if __name__ == "__main__":
+    req = request.Request(sys.argv[1])
     try:
-        with request.urlopen(sys.argv[1]) as respose:
-            res = respose.read()
+        with request.urlopen(req) as response:
+            res = response.read()
             print(res.decode("utf-8"))
     except error.HTTPError:
-        print("Error code: {}", respose.status_code)
+        print("Error code: {}", req.status)
