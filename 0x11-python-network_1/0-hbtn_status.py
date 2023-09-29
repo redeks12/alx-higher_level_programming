5e7bd8f62f169c6a333750910450ff519f38b6e2
kumdan job <redeks123456@gmail.com>
2023-09-29 10:30:41 +0100

    m

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,4 +3,8 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        print(response)
+        res = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(res)))
+        print("\t - content: {}".format(res))
+        print("\t - utf8 content: {}".format(res.decode("utf8")))
