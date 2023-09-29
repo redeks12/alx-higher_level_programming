b6b0523de6255f5f623d6e246d4e2fb5e211a56e
kumdan job <redeks123456@gmail.com>
2023-09-29 10:40:31 +0100

    m

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[0]) as response:
+        res = response.getheaders()
+        print(res.get("X-Request-Id"))
