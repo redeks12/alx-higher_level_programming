35a87cd35b412b3bcf714adf0cd8e4c1709621cc
kumdan job <redeks123456@gmail.com>
2023-09-29 10:59:13 +0100

    m

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     params = {"email": sys.argv[2]}
     data = parse.urlencode(params)
+    data = data.encode("ascii")
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
         res = response.read()
