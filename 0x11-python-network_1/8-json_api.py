f74e57d7717edeed4e95711ef2a7d0315731ef6a
kumdan job <redeks123456@gmail.com>
2023-09-29 11:46:22 +0100

    m

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    if sys.argv[1]:
+        param = {"q": sys.argv[1]}
+    else:
+        param = {"q": ""}
+    response = requests.post("http://0.0.0.0:5000/search_user", params=param)
+    res = response.json()
+    try:
+        if res:
+            print("[{}] {}")
+        else:
+            print("No result")
+    except requests.JSONDecodeError:
+        print("Not a valid JSON")
