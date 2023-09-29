2d4e120f88ba2f87931e3bd47771daeb5f99aa9d
kumdan job <redeks123456@gmail.com>
2023-09-29 11:49:54 +0100

    m

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     response = requests.post("http://0.0.0.0:5000/search_user", params=param)
     res = response.json()
     print(res)
+    print(response.text)
     try:
         if res:
             print("[{}] {}".format(res["id"], res["name"]))
