986a9e1e6d9bfa3a6055fec12fd9132a5ade1e92
kumdan job <redeks123456@gmail.com>
2023-09-29 16:23:22 +0100

    m

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(
-        "https://api.github.com/users/{}".format(username),
-        headers={"accept": "application/vnd.github+json"},
+        "https://api.github.com/user",
         auth=HTTPBasicAuth(username, password),
     )
-    print(response.json()["id"])
+    res = response.json()
+    print(res.get("id"))
