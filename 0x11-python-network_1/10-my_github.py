f078bcef0272b9545d836949a01c2c3bcf321cb6
kumdan job <redeks123456@gmail.com>
2023-09-29 15:46:34 +0100

    m

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(
-        "https://api.github.com/user/{}".format(username),
+        "https://api.github.com/users/{}".format(username),
         headers={"password": password, "X-GitHub-Api-Version": "2022 - 11 - 28"},
     )
     print(response.json()["id"])
