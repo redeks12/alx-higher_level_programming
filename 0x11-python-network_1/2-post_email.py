dc049cf38dc8ab4aa08383c701f07e2aa2c03a48
kumdan job <redeks123456@gmail.com>
2023-09-29 10:56:51 +0100

    m

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
+from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
     params = {"email": sys.argv[2]}
-    url = "{}?{}".format(sys.argv[1], params)
-    with urllib.request.urlopen(url) as response:
+    data = parse.urlencode(params)
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
         res = response.read()
         print(res.decode("utf8"))
