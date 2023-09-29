0ea8c845724f7c6553ce2f1f43d60a6cb524003e
kumdan job <redeks123456@gmail.com>
2023-09-29 19:26:52 +0100

    m

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         res = response.read()
+
         print("Body response:")
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,13 +3,14 @@
 import sys
 
 import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     response = requests.get(
         "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
     )
+
     res = response.json()
+
     try:
         for resp in res[:10]:
             print("{}: {}".format(resp["sha"], resp["commit"]["author"]["name"]))
