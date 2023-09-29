1c450801d747f9b7971adb10d5cbbb4b78cc9556
kumdan job <redeks123456@gmail.com>
2023-09-29 11:37:07 +0100

    m

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import requests
 import sys
+
+import requests
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    print(response.status_code)
-    print(response.text)
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
