3688dfb460eea72f345df29ca4e4836154788395
kumdan job <redeks123456@gmail.com>
2023-09-29 16:32:49 +0100

    m

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches data"""
+import sys
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    response = requests.get(
+        "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    )
+    res = response.json()
+    for resp in res:
+        print("{}: {}".format(resp["sha"], resp["author"]["name"]))
